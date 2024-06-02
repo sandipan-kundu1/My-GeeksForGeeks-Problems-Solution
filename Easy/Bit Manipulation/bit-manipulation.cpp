67d4b6323f5cd8434c4e1ddb2f10070c1c9c1dbb
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2024-06-03 00:16:41 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Bit Manipulation/bit-manipulation.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void bitManipulation(int num, int i) {
+        // your code here
+        auto shift= 1 <<(i-1);
+        if((num & shift)) 
+        cout<<1<<" ";
+        else
+        cout<<0<<" ";
+        cout<< (num | shift)<<" ";
+        cout<< (num & (~shift));
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n >> i;
+        Solution ob;
+        ob.bitManipulation(n, i);
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
