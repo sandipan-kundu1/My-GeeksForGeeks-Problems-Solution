3652e12ad9fcfb3b01a6a447f9ef4fc02927f914
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2024-12-26 20:17:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Geek Jump/geek-jump.cpp
+++ Difficulty: Easy/Geek Jump/geek-jump.cpp
@@ -6,35 +6,19 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int func(vector<int>& dp,vector<int>& height,int i)
-    {
-        if(i==0)
-        return 0;
-        
-        int one,two;
-        if(dp[i]!=INT_MAX)
-        return dp[i];
-        
-        one=func(dp,height,i-1)+ abs(height[i]-height[i-1]);
-        if(i==1)
-        dp[i]=one;
-        else
-        {
-            two=func(dp,height,i-2)+ abs(height[i]-height[i-2]);
-            dp[i]=min(one,two);
-        }
-        
-        
-        return dp[i];
-    }
     int minimumEnergy(vector<int>& height, int n) {
         // Code here
         if(n==1)
         return 0;
         
-        vector<int> dp(n,INT_MAX);
-        
-        return func(dp,height,n-1);
+        vector<int> dp(n,-1);
+        dp[0]=0;
+        dp[1]=abs(height[1]-height[0]);
+        for(int i=2;i<n;i++)
+        {
+            dp[i]=min(dp[i-1]+ abs(height[i]-height[i-1]), dp[i-2]+ abs(height[i]-height[i-2]));
+        }
+        return dp[n-1];
     }
 };
 
