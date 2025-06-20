5130521d0c3aef7a543f8e9a32cb625d9bc0d6ba
Sandipan Kundu <158579989+sandipan-kundu1@users.noreply.github.com>
2025-06-21 03:16:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -1,32 +1,66 @@
+class DSU
+{
+    vector<int> rank,parent,size;
+public:
+    DSU(int n)
+    {
+        rank.resize(n+1,0);
+        size.resize(n+1,1);
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++)
+            parent[i]=i; // initially each node is its own parent
+    }
+    int findupar(int node)
+    {
+        if(parent[node]==node)
+            return node;
+        return parent[node]=findupar(parent[node]); // recursively find the parent
+    }
+    void unionbysize(int u,int v)
+    {
+        int upu=findupar(u);
+        int upv=findupar(v);
+
+        if(upu==upv)  return;
+
+        if(size[upu]<size[upv])
+        {
+            parent[upu]=upv;
+            size[upv]+=size[upu];
+        }
+        else
+        {
+            parent[upv]=upu;
+            size[upu]+=size[upv];
+        }
+    }
+};
 class Solution {
   public:
     // Function to find sum of weights of edges of the Minimum Spanning Tree.
+    
     int spanningTree(int V, vector<vector<int>> adj[]) {
-        // code here
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
-        vector<int> vis(V,0);
-        pq.push({0,0}); //wt,node
-        vis[0]=1;
-        int sum=0;
-        while(!pq.empty())
+        vector<pair<int,pair<int,int>>> edges;
+        for(int u=0;u<V;u++)
         {
-            auto p=pq.top();
-            pq.pop();
-            int w=p[0],node=p[1];
-            if(!vis[node])
+            for(auto j:adj[u])
             {
-                sum+=w;
-                vis[node]=1;
+                int wt=j[1],v=j[0];
+                if(u<v)
+                edges.push_back({wt,{u,v}});
             }
-            
-            for(auto n:adj[node])
+        }
+        sort(edges.begin(),edges.end());
+        DSU dsu(V);
+        int sum=0;
+        for(auto i:edges)//Kruskal's algo
+        {
+            auto p=i.second;
+            int wt=i.first,u=p.first,v=p.second;
+            if(dsu.findupar(u)!=dsu.findupar(v))
             {
-                int nei=n[0],wt=n[1];
-                if(!vis[nei])
-                {
-                    pq.push({wt,nei});
-                    
-                }
+                dsu.unionbysize(u,v);
+                sum+=wt;
             }
         }
         return sum;
